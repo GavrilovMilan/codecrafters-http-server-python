0beabcdf9d6b343ae9a534ee7e58e724a759e74f
Milan <milangavrilov01@gmail.com>
2023-10-19 21:10:31 +0200

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import socket
 
 
+HTTP_200 = "HTTP/1.1 200 OK\r\n\r\n".encode()
+HTTP_404 = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
@@ -10,12 +12,14 @@ def main():
 
     conn, addr = server_socket.accept()
     data = conn.recv(1024).decode()
-    path = data[0]
+    path = data.split()[1]
     print(data)  # b'GET / HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: Go-http-client/1.1\r\nAccept-Encoding: gzip\r\n\r\n'
     print(path)
 
-    conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-
+    if path == '/':
+        conn.send(HTTP_200)
+    else:
+        conn.send(HTTP_404)
 
     server_socket.close()
 
