b08cdea46857fdd0292fb21ca7d8b52a0eef86a0
Milan <milangavrilov01@gmail.com>
2023-10-19 18:24:23 +0200

    stage 2

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept()
 
+    conn, address = server_socket.accept()
+    data = conn.recv(1024)
+    conn.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
+
 
 if __name__ == "__main__":
     main()
