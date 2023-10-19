0eebb469fe146f0bd131c7aa929b781ada311452
Milan <milangavrilov01@gmail.com>
2023-10-19 20:41:37 +0200

    stage 2

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 import socket
 
 
-_HTTP_200_RESP = "HTTP/1.1 200 OK\r\n\r\n".encode()
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # print("Logs from your program will appear here!")
@@ -9,24 +8,10 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.accept()
 
-    server_socket.listen(5)
-    conn, addr = server_socket.accept()  # wait for client
+    conn, addr = server_socket.accept()
     data = conn.recv(1024)
-    print(data.decode("utf-8"))
-    conn.sendall(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
+    conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
     server_socket.close()
-
-    # server_socket.listen(5)
-    # (conn, _) = server_socket.accept()
-    # data = conn.recv(1024)
-    # print(data)
-    # conn.send(_HTTP_200_RESP)
-    # conn.close()
-
-    # with server_socket:
-    #     conn, _ = server_socket.accept()  # wait for client
-    #     conn.send("HTTP/1.1 200 OK\r\n\r\n".encode())
-
 
 if __name__ == "__main__":
     main()
