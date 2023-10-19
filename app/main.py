1e3a0f31a3adfe7f66ef696f1e36487d1b98d0b6
Milan <milangavrilov01@gmail.com>
2023-10-19 22:41:16 +0200

    stage 6

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 import socket
+import threading
 
 
 HTTP_200 = "HTTP/1.1 200 OK\r\n"
 HTTP_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
-    conn, addr = server_socket.accept()
+def handle_request(conn):
     data = conn.recv(1024).decode()
     path = data.split()[1]
     string = ''.join(path[6:])
@@ -14,15 +13,25 @@ def main():
     if path == '/':
         conn.send((HTTP_200 + '\r\n').encode())
     elif path.split('/')[1] == 'echo':
-        conn.send((HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(string)}\r\n\r\n' + string).encode())
+        conn.send(
+            (HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(string)}\r\n\r\n' + string).encode())
     elif path.split('/')[1] == 'user-agent':
         agent = data.split('\r\n')[2].split()[1]
         print(agent)
-        conn.send((HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(agent)}\r\n\r\n' + agent).encode())
+        conn.send(
+            (HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(agent)}\r\n\r\n' + agent).encode())
     else:
         conn.send(HTTP_404.encode())
 
-    server_socket.close()
+def main():
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    while True:
+        conn, addr = server_socket.accept()
+        t = threading.Thread(target=handle_request, args=(conn))
+        t.start()
+
+
 
 if __name__ == "__main__":
     main()
