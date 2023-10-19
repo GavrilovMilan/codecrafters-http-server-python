08869a91a06134acc2599f0c2c701b1298c22ccd
Milan <milangavrilov01@gmail.com>
2023-10-19 21:39:57 +0200

    stage 2

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
     conn, addr = server_socket.accept()
     data = conn.recv(1024).decode()
     path = data.split()[1]
-    string = ''.join(path.split('/')[2:])
+    string = ''.join(path[5:])
     print(path)
     print(string)
     if path == '/':
