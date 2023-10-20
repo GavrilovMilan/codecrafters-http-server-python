47669728e629e859c05717b5cb67d3be71fede5a
Milan <milangavrilov01@gmail.com>
2023-10-20 12:19:48 +0200

    stage 7

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import threading
 import os
 import sys
 
-
 HTTP_200 = "HTTP/1.1 200 OK\r\n"
 HTTP_404 = "HTTP/1.1 404 Not Found\r\n\r\n"
+
 
 def handle_request(conn):
     data = conn.recv(1024).decode()
@@ -20,17 +20,22 @@ def handle_request(conn):
     elif path.split('/')[1] == 'user-agent':
         agent = data.split('\r\n')[2].split()[1]
         # print(agent)
-        conn.send((HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(agent)}\r\n\r\n' + agent).encode())
+        conn.send(
+            (HTTP_200 + 'Content-Type: text/plain\r\n' + f'Content-Length: {len(agent)}\r\n\r\n' + agent).encode())
     elif path.split('/')[1] == 'files':
         filename = path.split('/')[2]
         dir = sys.argv[-1]
-        if filename in dir:
-            conn.send((HTTP_200 + 'Content-Type: application/octet-stream\r\n').encode())
+        filePath = dir + filename
+        if os.path.exists(filePath):
+            content = open(filePath).read()
+            conn.send((HTTP_200 + 'Content-Type: application/octet-stream\r\n' +
+                       f'Content-Length: {len(content)}\r\n\r\n' + content).encode())
         else:
             conn.send(HTTP_404.encode())
     else:
         conn.send(HTTP_404.encode())
     conn.close()
+
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
@@ -39,7 +44,6 @@ def main():
         conn, addr = server_socket.accept()
         t = threading.Thread(target=handle_request, args=(conn,))
         t.start()
-
 
 
 if __name__ == "__main__":
